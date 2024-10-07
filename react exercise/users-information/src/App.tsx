5b019ff3f71ccf1f8a3031da98464114c64c27ac
shirashko <shirashko@gmail.com>
2024-10-07 17:19:26 +0300

    basic project structure

--- react exercise/users-information/src/App.tsx
+++ react exercise/users-information/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import UserList from './components/UserList/UserList.tsx'
+import UserList from './pages/UserList/UserList'
 
 function App() {
   const [count, setCount] = useState(0)
