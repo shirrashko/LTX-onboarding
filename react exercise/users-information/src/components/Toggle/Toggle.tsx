58f98fec151a72d6492046684f87a17744565697
shirashko <shirashko@gmail.com>
2024-10-08 13:50:38 +0300

    toggle & search components

--- react exercise/users-information/src/components/Toggle/Toggle.tsx
+++ react exercise/users-information/src/components/Toggle/Toggle.tsx
@@ -1,25 +1,23 @@
-import React, { useState } from 'react';
-import listIcon from '../../assets/list-icon.svg';
-import gridIcon from '../../assets/grid-icon.svg';
-import './Toggle.scss';
+import React, { useState } from "react";
+import listIcon from "../../assets/list-icon.svg";
+import gridIcon from "../../assets/grid-icon.svg";
+import "./Toggle.scss";
 
 const Toggle = () => {
   const [isGridView, setIsGridView] = useState(true);
 
   const handleToggle = () => {
-    setIsGridView(!isGridView); // Toggle between Grid and List views
+    setIsGridView(!isGridView);
   };
 
   return (
-    <div className="toggle">
-      <div
-        className={`toggle-button ${isGridView ? 'grid-active' : 'list-active'}`}
-        onClick={handleToggle}
-      >
-        <span className="toggle-ellipse" />
-        <img src={gridIcon} alt="Grid Icon" className="grid-icon" />
-        <img src={listIcon} alt="List Icon" className="list-icon" />
-      </div>
+    <div
+      className={`toggle ${isGridView ? "grid-active" : "list-active"}`}
+      onClick={handleToggle}
+    >
+      <span className="toggle-ellipse" />
+      <img src={gridIcon} alt="Grid Icon" className="grid-icon" />
+      <img src={listIcon} alt="List Icon" className="list-icon" />
     </div>
   );
 };
--- react exercise/users-information/src/components/search/Search.tsx
+++ react exercise/users-information/src/components/search/Search.tsx
@@ -1,19 +1,34 @@
-import searchIcon from '../../assets/search-icon.svg';
-import './Search.scss';
+import React, { useState } from "react";
+import searchIcon from "../../assets/search-icon.svg";
+import deleteIcon from "../../assets/delete-icon.svg";
+import "./Search.scss";
 
 function Search() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearInput = () => {
+    setInputValue("");
+  };
+
   return (
-    <div className='search'>
+    <div className="search">
       <input
-        className='search-input'
+        className="search-input"
         type="text"
         placeholder="Placeholder"
+        value={inputValue}
+        onChange={handleInputChange}
       />
-      <img
-        src={searchIcon}
-        alt="Search Icon"
-        className="search-icon"
-      />
+      <img src={searchIcon} alt="Search Icon" className="search-icon" />
+      {inputValue && (
+        <div className="delete-icon-container" onClick={clearInput}>
+          <img src={deleteIcon} alt="Delete Icon" className="delete-icon" />
+        </div>
+      )}
     </div>
   );
 }
