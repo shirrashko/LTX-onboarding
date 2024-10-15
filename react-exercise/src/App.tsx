4a1ea83b6b86ab962609c5bfef29ae5c48d3ef1b
shirashko <shirashko@gmail.com>
2024-10-15 10:21:09 +0300

    adding basic routing

--- react-exercise/src/App.tsx
+++ react-exercise/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import UserProfile from "./pages/userProfile/UserProfile.tsx";
+import UserDetails from "./pages/usersDetails/UsersDetails.tsx";
 import { User } from "./types/user.ts";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
@@ -11,9 +13,23 @@ function App() {
       .then((data) => setUsers(data.users));
   }, []);
   return (
-    <>
-      {users.length > 0 ? <UserProfile user={users[0]} /> : <p>Loading...</p>}
-    </>
+    <Router>
+      <Routes>
+        <>
+          <Route path="/" element={<UserDetails users={users} />} />
+          <Route
+            path="/user-profile"
+            element={
+              users.length > 0 ? (
+                <UserProfile user={users[0]} />
+              ) : (
+                <p>Loading...</p>
+              )
+            }
+          />
+        </>
+      </Routes>
+    </Router>
   );
 }
 export default App;
