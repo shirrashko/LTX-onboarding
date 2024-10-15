adba90ecd9d276fd2e4377cf6e5232e5d3fc12a8
shirashko <shirashko@gmail.com>
2024-10-15 11:24:45 +0300

    User profile page: back to search

--- react-exercise/src/pages/userProfile/UserProfile.tsx
+++ react-exercise/src/pages/userProfile/UserProfile.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 import Topper from "../../components/topper/Topper";
 import { User } from "../../types/user.ts";
 import defaultProfilePicture from "../../assets/default-profile-picture.svg";
 import Tabs from "../../components/tabs/Tabs.tsx";
+import backArrowIcon from "../../assets/back-arrow-icon.svg";
 import "./UserProfile.scss";
 
 interface UserProfileProps {
@@ -12,30 +14,41 @@ function UserProfile({ user }: UserProfileProps) {
   return (
     <div className="user-profile">
       <Topper />
-      <div className="page-content">
-        <div className="profile-section">
-          <div className="user-preview">
-            <img
-              className="profile-image"
-              src={user.image || defaultProfilePicture}
-              alt={`${user.firstName} ${user.lastName}'s profile picture`}
-            />
-            <div className="user-details">
-              <h2>
-                {user.firstName} {user.lastName}
-              </h2>
-              <span>
-                {user.age}. {user.gender}. {user.address.city}.{" "}
-                {user.address.state}, {user.address.country}
-              </span>
-              <span>{user.email}</span>
-              <div className="change-details">
-                <button className="edit-button">Edit</button>
+
+      <div className="container">
+        <Link to="/" className="back-to-search">
+          <img
+            src={backArrowIcon}
+            alt="Back to search"
+            className="back-arrow-icon"
+          />
+          Back to search
+        </Link>
+        <div className="page-content">
+          <div className="profile-section">
+            <div className="user-preview">
+              <img
+                className="profile-image"
+                src={user.image || defaultProfilePicture}
+                alt={`${user.firstName} ${user.lastName}'s profile picture`}
+              />
+              <div className="user-details">
+                <h2>
+                  {user.firstName} {user.lastName}
+                </h2>
+                <span>
+                  {user.age}. {user.gender}. {user.address.city}.{" "}
+                  {user.address.state}, {user.address.country}
+                </span>
+                <span>{user.email}</span>
+                <div className="change-details">
+                  <button className="edit-button">Edit</button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="user-content">
-            <Tabs user={user} />
+            <div className="user-content">
+              <Tabs user={user} />
+            </div>
           </div>
         </div>
       </div>
